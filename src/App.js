23816fca5ca88b8b6531b7e07b20532fac472f58
Navpreet <navpreet.boparai@prepladder.com>
2022-05-13 21:13:21 +0530

    navigation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,59 @@
 import "./App.css";
 import HomePage from "./pages/homepage/Homepage";
+import {
+  Routes,
+  Route,
+  useLocation,
+  useParams,
+  Link,
+  useNavigate,
+} from "react-router-dom";
+
+// const About = (props) => {
+//   console.log(props);
+//   return <h1>About Us</h1>;
+// };
+// const Topic = (props) => {
+//   console.log(useLocation());
+//   let navigate = useNavigate();
+//   return (
+//     <div>
+//       <h1>Topic </h1>
+//       <Link to="9">Take me to number 9 </Link>
+//       <button
+//         onClick={() => {
+//           console.log(navigate, "navigate");
+//           navigate("3", { replace: true });
+//         }}
+//       >
+//         Take me to number 3
+//       </button>
+//     </div>
+//   );
+// };
+
+// const TopicId = (props) => {
+//   console.log(useLocation());
+//   console.log("matchPath", useParams(), props);
+//   return <h1>Topic ID </h1>;
+// };
+
+const Hats = () => (
+  <div>
+    <h1>Hats </h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Routes>
+        <Route exact path="/" element={<HomePage />} />
+        {/* <Route path="about" element={<About />} />
+        <Route path="topic" element={<Topic />} />
+        <Route path="topic/:topicId" element={<TopicId />} /> */}
+        <Route path="hats" element={<Hats />} />
+      </Routes>
     </div>
   );
 }
--- src/components/menu-items/MenuItem.js
+++ src/components/menu-items/MenuItem.js
@@ -1,25 +1,37 @@
 import React from "react";
-// import { withRouter } from "react-router-dom";
-
 import "./MenuItem.scss";
+import { useLocation, useNavigate, useParams } from "react-router-dom";
 
-const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
-  <div
-    className={`${size} menu-item`}
-    onClick={() => history.push(`${match.url}${linkUrl}`)}
-  >
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const params = useParams();
+
+  console.log("location", location);
+  console.log("navigate", navigate);
+  console.log("params", params);
+
+  return (
     <div
-      className="background-image"
-      style={{
-        backgroundImage: `url(${imageUrl})`,
+      className={`${size} menu-item`}
+      // onClick={() => history.push(`${match.url}${linkUrl}`)}
+      onClick={() => {
+        console.log("linkUrl", linkUrl);
+        navigate(`${linkUrl}`);
       }}
-    />
-    <div className="content">
-      <h1 className="title">{title.toUpperCase()}</h1>
-      <span className="subtitle">SHOP NOW</span>
+    >
+      <div
+        className="background-image"
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      />
+      <div className="content">
+        <h1 className="title">{title.toUpperCase()}</h1>
+        <span className="subtitle">SHOP NOW</span>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
-// export default withRouter(MenuItem);
 export default MenuItem;
