3aa2fa8c21e7c7a2b49b315904d2c657a2fef348
Navpreet <navpreet.boparai@prepladder.com>
2022-05-21 17:01:45 +0530

    sign in

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import Shop from "./pages/shop/shop";
 import Header from "./components/header/Header";
 import Credentials from "./pages/credentials/Credentials.js";
 
-import { auth } from "./firebase/firebase.utils";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
 class App extends Component {
   constructor() {
@@ -26,9 +26,24 @@ class App extends Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
-      console.log(user);
-      this.setState({ currentUser: user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot((snapShot) => {
+          this.setState(
+            {
+              currentUser: {
+                id: snapShot.id,
+                ...snapShot.data(),
+              },
+            },
+            () => console.log(this.state.currentUser)
+          );
+        });
+      } else {
+        this.setState({ currentUser: userAuth });
+      }
     });
   }
 
